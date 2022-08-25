9ad0c84b111efb058e0d41a3233134ae49e7e58a
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-08-25 18:34:09 +0200

    finish: product cards

---
+++ src/components/SingleProduct.vue
@@ -0,0 +1,40 @@
+<template>
+    <a href="#" class="product-card">
+        <img :src="thumb" />
+        <h3>{{series}}</h3>
+    </a>
+</template>
+
+<script>
+
+export default {
+    name: "SingleProduct",
+    props: {
+        thumb: String,
+        series: String
+    }
+}
+</script>
+
+<style lang="scss">
+    .product-card {
+
+        flex-basis: 15%;
+        padding: 10px;
+        margin-bottom: 1rem;
+        text-align: center;
+        margin: auto;
+        text-decoration: none;
+
+        img {
+            max-width: 100%;
+            margin-bottom: 1rem;
+        }
+
+        h3 {
+            font-size: 12px;
+            color: #fff;
+        }
+
+    }
+</style>
