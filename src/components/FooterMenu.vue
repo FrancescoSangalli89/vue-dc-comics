d872a5a5b29a486e6fd27839ba0ab9b1b78c5757
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-08-25 16:16:35 +0200

    finish: base layout

---
+++ src/components/FooterMenu.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="footer-links">
+        <h4>{{title}}</h4>
+        <ul>
+            <li v-for="(url, index) in urls" :key="index">
+                <a :href="url.url">{{url.text}}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FooterMenu",
+    props: {
+        title: String,
+        urls: Array
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .footer-links {
+        margin: 0 1rem;
+        h4 {
+            color: #fff;
+            margin-bottom: 1rem;
+        }
+        ul {
+            list-style: none;
+            li {
+                margin-bottom: 0.5rem;
+
+                a {
+                text-decoration: none;
+                color: gray;
+                
+                }
+            }
+            
+        }
+    }
+    
+</style>
--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -2,55 +2,205 @@
     <footer>
         <div class="footerTop">
             <nav>
-                <ul class="HeaderMenu">
+                <ul class="middleMenu">
                     <li v-for="(link, index) in links" :key="index" >
                         <img :src="link.icon" />
-                        <a :href="link.url" :class="link.active?'active':''" >{{link.text}}</a>
+                        <a :href="link.url" >{{link.text}}</a>
                     </li>
                 </ul>
             </nav>
         </div>
         <div class="footerBtm">
-
+            <div class="container">
+                <FooterMenu :title="'DC COMICS'" :urls="dcComicsLinks" />
+                <FooterMenu :title="'SHOP'" :urls="shopLinks" />
+                <FooterMenu :title="'DC'" :urls="dcLinks" />
+                <FooterMenu :title="'SITES'" :urls="siteLinks" />
+                <img src="../assets/images/dc-logo-bg.png" alt="dc footer logo" />
+            </div>
+        </div>
+        <div class="pageEnding">
+            <div class="container">
+                <button>SIGN-UP NOW!</button>
+                <div class="social-icons">
+                    <h4>FOLLOW US</h4>
+                    <ul>
+                        <li v-for="(icon, index) in icons" :key="index" >
+                            <img :src="icon.icon" />
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </footer>
 </template>
 
 <script>
+
+import FooterMenu from './FooterMenu.vue';
+
 export default {
     name: 'MyFooter',
+    components: {
+    FooterMenu
+},
     data() {
         return {
             links: [
                 {
                     text: 'DIGITAL COMICS',
                     url: '#',
-                    icon: '@/assets/images/buy-comics-digital-comics.png',
+                    icon: require('../assets/images/buy-comics-digital-comics.png'),
                     active: false
                 },
                 {
                     text: 'DC MERCHANDISE',
                     url: '#',
-                    icon: '@/assets/images/buy-comics-merchandise.png',
+                    icon: require('@/assets/images/buy-comics-merchandise.png'),
                     active: false
                 },
                 {
                     text: 'SUBSCRIPTION',
                     url: '#',
-                    icon: '@/assets/images/buy-comics-subscriptions.png',
+                    icon: require('@/assets/images/buy-comics-subscriptions.png'),
                     active: false
                 },
                 {
                     text: 'COMIC SHOP LOCATOR',
                     url: '#',
-                    icon: '@/assets/images/buy-comics-shop-locator.png',
+                    icon: require('@/assets/images/buy-comics-shop-locator.png'),
                     active: false
                 },
                 {
                     text: 'DC POWER VISA',
                     url: '#',
-                    icon: '@/assets/images/buy-dc-power-visa',
+                    icon: require('@/assets/images/buy-dc-power-visa.svg'),
                     active: false
+                }
+            ],
+            dcComicsLinks: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                },
+                {
+                    text: 'news',
+                    url: '#',
+                }
+            ],
+            shopLinks: [
+                {
+                    url: '#',
+                    text: 'Shop DC'
+                },
+                {
+                    url: '#',
+                    text: 'Shop DC Collectibles'
+                },
+            ],
+            dcLinks: [
+                {
+                    text: 'Terms Of Use',
+                    url: '#',
+                },
+                {
+                    text: 'Privacy policy (New)',
+                    url: '#',
+                },
+                {
+                    text: 'Ad Choices',
+                    url: '#',
+                },
+                {
+                    text: 'Advertising',
+                    url: '#',
+                },
+                {
+                    text: 'Jobs',
+                    url: '#',
+                },
+                {
+                    text: 'Subscriptions',
+                    url: '#',
+                },
+                {
+                    text: 'Talent Workshops',
+                    url: '#',
+                },
+                {
+                    text: 'CPSC Certificates',
+                    url: '#',
+                },
+                {
+                    text: 'Ratings',
+                    url: '#',
+                },
+                {
+                    text: 'Shop Help',
+                    url: '#',
+                },
+                {
+                    text: 'Contact Us',
+                    url: '#',
+                }
+            ],
+            siteLinks: [
+                {
+                    text: 'DC',
+                    url: '#',
+                },
+                {
+                    text: 'MAD Magazine',
+                    url: '#',
+                },
+                {
+                    text: 'DC Kids',
+                    url: '#',
+                },
+                {
+                    text: 'DC Universe',
+                    url: '#',
+                },
+                {
+                    text: 'DC Power Visa',
+                    url: '#',
+                }
+            ],
+            icons: [
+                {
+                    icon: require('@/assets/images/footer-facebook.png')
+                },
+                {
+                    icon: require('@/assets/images/footer-periscope.png')
+                },
+                {
+                    icon: require('@/assets/images/footer-pinterest.png')
+                },
+                {
+                    icon: require('@/assets/images/footer-twitter.png')
+                },
+                {
+                    icon: require('@/assets/images/footer-youtube.png')
                 }
             ]
         }
@@ -83,6 +233,12 @@ export default {
                     font-weight: 600;
                     line-height: 115px;
 
+                    img {
+                        height: 50px;
+                        margin-right: 10px;
+                        vertical-align: middle;
+                    }
+
                     a {
                         text-decoration: none;
                         color: #fff;
@@ -91,5 +247,61 @@ export default {
                 }
             }
         }
+    }
+
+    .footerBtm {
+        height: 480px;
+        background-image: url('../assets/images/footer-bg.jpg');
+        padding: 2rem;
+        
+        .container {
+            display: flex;
+
+            img {
+                height: 440px;
+            }
+        }
+    }
+
+    .pageEnding {
+        height: 105px;
+        width: 100%;
+        background-color: $footer-color;
+        line-height: 105px;
+        
+        .container {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            button {
+                background-color: rgba($color: #fff, $alpha: 0);
+                color: #fff;
+                border: 2px $brand-color solid;
+                padding: 1rem;
+            }
+
+            .social-icons {
+                display: flex;
+
+                h4 {
+                    vertical-align: middle;
+                    color: $brand-color;
+                    margin-right: 1rem;
+                }
+
+                ul {
+                    display: flex;
+                    list-style: none;
+
+                        img {
+                            vertical-align: middle;
+                            padding: 0 0.5rem;
+                        }
+                }
+            }
+        }
+
+        
     }
 </style>
